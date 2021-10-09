d8fb4d542378862bd8c1dbf6f294a6d0459e095f
Anna Papitto <annapapitto@gmail.com>
2021-10-08 18:02:39 -0700

    Subcommands as modules

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,53 @@
-use clap::{App, Arg, SubCommand};
+use clap::App;
 
 fn main() {
     println!("Hello, world! Let's be good friends.");
 
-    let list_sub = SubCommand::with_name("list")
-            .about("List all of your friends");
-    let add_sub = SubCommand::with_name("add")
-            .about("Add a friend")
-            .arg(Arg::with_name("friend")
-                .help("Name of the friend")
-                .required(true));
+    let subcommands = vec![
+        list::as_subcommand(),
+        add::as_subcommand()
+    ];
 
     let app = App::new("Friend Grow")
         .version("0.1")
         .author("annapapitto")
         .about("Let your friendships grow")
-        .subcommands(vec![ list_sub, add_sub ]);
+        .subcommands(subcommands);
     let matches = app.get_matches();
 
     if let Some(_) = matches.subcommand_matches("list") {
-        println!("Listing all of your friends...");
+        list::execute();
+    }
+    if let Some(matches) = matches.subcommand_matches("add") {
+        add::execute(matches);
+    }
+}
+
+mod list {
+    use clap::{App, SubCommand};
+
+    pub fn as_subcommand<'a, 'b>() -> App<'a, 'b> {
+        SubCommand::with_name("list")
+            .about("List all of your friends")
     }
 
-    if let Some(matches) = matches.subcommand_matches("add") {
+    pub fn execute() {
+        println!("Listing all of your friends...");
+    }
+}
+
+mod add {
+    use clap::{App, Arg, ArgMatches, SubCommand};
+
+    pub fn as_subcommand<'a, 'b>() -> App<'a, 'b> {
+        SubCommand::with_name("add")
+            .about("Add a friend")
+            .arg(Arg::with_name("friend")
+                .help("Name of the friend")
+                .required(true))
+    }
+
+    pub fn execute(matches: &ArgMatches) {
         println!("Adding a friend {}...", matches.value_of("friend").unwrap());
     }
 }
