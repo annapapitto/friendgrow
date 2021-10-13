eb6ea94273bd50b4fa80b663f88433da201ddee1
Anna Papitto <annapapitto@gmail.com>
2021-10-13 16:36:04 -0700

    First stab rusqlite

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use rusqlite::{params, Connection, Result};
 use structopt::StructOpt;
 
-fn main() {
+fn main() -> Result<()> {
     println!("Hello, world! Let's be good friends.");
 
     let opt = Friendgrow::from_args();
-    execute_subcommand(opt);
+
+    // TODO make this an arg
+    let conn = Connection::open("~/friends/friends.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS friend (
+            id integer primary key,
+            name text not null unique,
+            freq integer,
+            last_seen text,
+        )",
+        [],
+    )?;
+
+    execute_subcommand(opt, conn)?;
+    Ok(())
 }
 
 #[derive(StructOpt)]
@@ -22,21 +37,33 @@ enum Friendgrow {
     AddFriend { friend: String },
 }
 
-fn execute_subcommand(opt: Friendgrow) {
+#[derive(Debug)]
+struct Friend {
+    id: i32,
+    name: String,
+    freq: u8,             // TODO try with u8
+    last_hangout: String, // TODO should this be Option if no 'not null'?
+}
+
+fn execute_subcommand(opt: Friendgrow, conn: Connection) -> Result<()> {
     match opt {
         Friendgrow::ListFriends {} => {
-            list_friends();
+            return list_friends(conn);
         }
         Friendgrow::AddFriend { friend } => {
-            add_friend(friend);
+            return add_friend(friend, conn);
         }
     }
 }
 
-fn list_friends() {
+fn list_friends(conn: Connection) -> Result<()> {
     println!("Listing all of your friends...");
+    let names = conn.execute("SELECT name FROM friend", [])?;
+    println!("names: {:?}", names);
+    Ok(())
 }
 
-fn add_friend(friend: String) {
+fn add_friend(friend: String, conn: Connection) -> Result<()> {
     println!("Adding a friend {}...", friend);
+    Ok(())
 }
